0b016a89448c3a5e8eec4684af48650c6da32cbd
besaklish <besaklish@gmail.com>
2018-12-11 18:14:46 +0900

    add PreloadJS for sophisticated loading

--- docs/works/js/drink_tapioka.js
+++ docs/works/js/drink_tapioka.js
@@ -1,34 +1,65 @@
-function init() {
-    var stage = new createjs.Stage("demoCanvas");
+// define variables to be used inside several functions.
+var queue, stage, manifest;
+var bitmap_tea, bitmap_perl, bitmap_guild, button;
 
-    // add a bitmap (tapioka_tea.png and tapioka_perl.jpg)
-    var bitmap_tea = new createjs.Bitmap("../images/tapioka_tea.png");
+// PreloadJS
+function init() {
+    queue = new createjs.LoadQueue();
+
+    queue.addEventListener('complete', completeHandler);
+
+    manifest = [
+        {
+            id: 'bitmap_tea',
+            src:'../images/tapioka_tea.png'
+        },
+        {
+            id: 'bitmap_perl',
+            src:'../images/tapioka_perl.png'
+        },
+        {
+            id: 'bitmap_guild',
+            src:'../images/toki.jpg'
+        }
+    ];
+
+    queue.setMaxConnections(3);
+    queue.loadManifest(manifest);
+}
+
+// after PreloadJS complete its task, move on to EaselJS
+function completeHandler(event){
+    stage = new createjs.Stage("demoCanvas");
+
+    bitmap_tea = new createjs.Bitmap(manifest[0].src);
     bitmap_tea.image.onload = function(){
         bitmap_tea.setTransform(250, 75, 0.25, 0.25);
         bitmap_tea.regX = bitmap_tea.getTransformedBounds().width / 2;
         bitmap_tea.regY = bitmap_tea.getTransformedBounds().height /2;
         stage.update();
-    }
+    };
 
-    var bitmap_perl = new createjs.Bitmap("../images/tapioka_perl.png");
+    bitmap_perl = new createjs.Bitmap(manifest[1].src);
     bitmap_perl.setTransform(200, 100, 0.2, 0.2);
     bitmap_perl.regX = bitmap_perl.image.width / 2;
     bitmap_perl.regY = bitmap_perl.image.height /2;
     bitmap_perl.visible = false;
     bitmap_perl.image.onload = function(){
         stage.update();
-    }
+    };
 
-    var bitmap_guild = new createjs.Bitmap("../images/toki.jpg");
+    bitmap_guild = new createjs.Bitmap(manifest[2].src);
     bitmap_guild.setTransform(200, 250, 0.25, 0.25);
     bitmap_guild.regX = bitmap_guild.image.width /2;
     bitmap_guild.regY = bitmap_guild.image.height/2;
     bitmap_guild.image.onload = function(){
+        // bound_guild is to be used for "handleUp" eventHandler
+        bound_guild = bitmap_guild.getTransformedBounds();
         stage.update();
-    }
+    };
 
     // add a button (for making thig bigger)
-    var button = new createjs.Container();
+    button = new createjs.Container();
     button.cursor = "pointer";
     button.x = 100;
     button.y = 100;
@@ -54,53 +85,6 @@ function init() {
     bitmap_tea.on("pressup", handleUp);
     bitmap_perl.on("mousedown", handleDown);
     bitmap_perl.on("pressmove", handleMove);
-
-    // EventListenrs
-    var drag_x;
-    var drag_y;
-    var drag_regX;
-    var drag_regY;
-    function handleDown(event){
-        drag_x = stage.mouseX - event.target.x;
-        drag_y = stage.mouseY - event.target.y;
-        drag_regX = stage.mouseX - event.target.regX;
-        drag_regY = stage.mouseY - event.target.regY; 
-    }
-
-    function handleMove(event){
-        event.target.x = event.stageX - drag_x;
-        event.target.y = event.stageY - drag_y;
-        event.target.regX = event.stageX - drag_regX;
-        event.target.regY = event.stageY - drag_regY;
-        stage.update();
-    }
-
-    function handleClick(event){
-        bitmap_tea.scale *= 1.1;
-        stage.update();
-    }
-
-    function handleTick(event){
-        if(!event.paused){
-            stage.update();
-        }
-    }
-
-    var bound_guild = bitmap_guild.getTransformedBounds();
-    function handleUp(event){
-        if (bound_guild.contains(bitmap_tea.x, bitmap_tea.y)){
-            var poo = bitmap_perl.clone()
-            poo.visible = true;
-            poo.alpha = 0;
-            poo.x = bound_guild.x + bound_guild.width / 2;
-            poo.y = bound_guild.y + bound_guild.height;
-            createjs.Tween.get(poo).to({alpha:1}, 3000).to({y:poo.y+10}, 1000);
-            poo.on("pressmove", handleMove);
-            stage.addChild(poo);
-            stage.update();
-        }
-        stage.update();
-    }
 
     // addEventListener: tick / drag / click
     createjs.Ticker.addEventListener("tick", handleTick);
@@ -128,3 +112,67 @@ function init() {
 
     stage.update();
 }
+
+var drag_x;
+var drag_y;
+var drag_regX;
+var drag_regY;
+function handleDown(event){
+    drag_x = stage.mouseX - event.target.x;
+    drag_y = stage.mouseY - event.target.y;
+}
+
+function handleMove(event){
+    event.target.x = event.stageX - drag_x;
+    event.target.y = event.stageY - drag_y;
+    stage.update();
+}
+
+function handleClick(event){
+    bitmap_tea.scale *= 1.1;
+    stage.update();
+}
+
+function handleTick(event){
+    if(!event.paused){
+        stage.update();
+    }
+}
+
+// look near bitmap_guild consructor for bound_guild
+var bound_guild;
+var poo_array = [];
+function handleUp(event){
+    if (bound_guild.contains(bitmap_tea.x+bitmap_tea.regX, bitmap_tea.y+bitmap_tea.regY)){
+        // Thig pooes!
+        var poo = bitmap_perl.clone();
+        poo.visible = true;
+        poo.alpha = 0;
+        poo.x = bound_guild.x + bound_guild.width / 2;
+        poo.y = bound_guild.y + bound_guild.height;
+        poo.on("pressmove", handleMove);
+
+        // a poo goes down because of the laws of gravity.
+        createjs.Tween.get(poo).to({y:poo.y+10, alpha:1}, 1000);
+
+        // move the bitmap_tea back to the original position.
+        createjs.Tween.get(bitmap_tea).to({x:250, y:75}, 1000);
+        
+        // add the poo to an array.
+        poo_array.push(poo);
+
+        stage.addChild(poo);
+        stage.update();
+    }
+
+    // If there are too much pooes on the screen, let's creen them!
+    if (poo_array.length === 7){
+        alert("The screen is too dirty.\nThis page reloads itself automatically.");
+        window.location.reload();
+    }
+    else if (poo_array.length === 5){
+        alert("You let Thig poo too many times.\nReload and clean the screen.");
+    }
+    stage.update();
+}
+
